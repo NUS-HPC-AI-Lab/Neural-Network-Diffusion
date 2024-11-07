744c79ee994577b3928a4350b27fbf6b8c5f754e
DongwenTang <MTDovent@gmail.com>
2024-11-07 08:33:39 +0800

    update full.

--- workspace/run.sh
+++ workspace/run.sh
@@ -4,16 +4,15 @@ tag=$1
 device=$2
 
 
-# cd ..
+cd ..
 echo "Processing: full/$tag"
 
-# cd "./dataset/full/$tag" || exit
-# CUDA_VISIBLE_DEVICES="$device" python train.py
-# CUDA_VISIBLE_DEVICES="$device" python finetune.py
+cd "./dataset/full/$tag" || exit
+CUDA_VISIBLE_DEVICES="$device" python train.py
+CUDA_VISIBLE_DEVICES="$device" python finetune.py
 
-# cd "../../../workspace" || exit
+cd "../../../workspace" || exit
 bash launch.sh "$tag" "$device"
 CUDA_VISIBLE_DEVICES="$device" python generate.py "$tag"
 CUDA_VISIBLE_DEVICES="$device" python evaluate.py "$tag"
-
-cd ..
+acd ..
