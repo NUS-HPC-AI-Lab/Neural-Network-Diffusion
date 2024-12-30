07ddca86d95ce2a34ef547e32eeb5c8f747f40bc
Kai Wang (Victor Kai) <37533040+kaiwang960112@users.noreply.github.com>
2024-12-30 20:10:26 +0800

    Delete workspace/run_stl10.sh

--- workspace/run_stl10.sh
+++
@@ -1,5 +0,0 @@
-bash run_all.sh main stl10_resnet50 2
-bash run_all.sh main stl10_vittiny 2
-bash run_all.sh main stl10_vitbase 2
-bash run_all.sh main stl10_convnexttiny 2
-bash run_all.sh main stl10_convnextbase 2
